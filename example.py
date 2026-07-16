ee0473630985b3eaa7cccfdba3584745cd8595b4
Juan Santhosh <juansanthosh@gmail.com>
2026-07-16 09:38:05 +0530

    Add comments to example code.

--- example.py
+++ example.py
@@ -10,18 +10,26 @@ MINUTES_PER_DAY = 60 * 24
 MINUTES_PER_MONTH = MINUTES_PER_DAY * 30
 MINUTES_PER_YEAR = MINUTES_PER_DAY * 365
 
-df = pd.read_csv("data/SOLUSDT-1.csv", sep="|", index_col=0)
-df = df.iloc[-MINUTES_PER_MONTH:].reset_index()
+# Load csv containing a column of close prices. This one has minute intervals
+df = pd.read_csv("data/SOLUSDT-1.csv", sep="|", index_col=0) 
+
+# Select the last month of data for example purposes
+df = df.iloc[-MINUTES_PER_MONTH:].reset_index() 
     
+# Create Backtester object
 backtester = Backtester(
-    df=df, close_column="close",
-    bars_per_year=MINUTES_PER_YEAR,
-    windows_per_year=12,
-    fee_rate=0.00001, fee_in_usd=False
+    df=df, close_column="close", # Name of close price column in your data
+    bars_per_year=MINUTES_PER_YEAR, 
+    windows_per_year=12, # 12 months per year
+    fee_rate=0.00001, fee_in_usd=False # False since we specify fees in SOL
 )
 
+# Example buy and hold strategy holding 2 SOL
 backtester.run(BuyAndHold(amount=2.0), n_mote_carlo_paths=1000)
+
+# Baseline strategy
 backtester.run(BuyAndHold(amount=1.0), n_mote_carlo_paths=0, baseline=True)
 
+# Display strategy performance
 backtester.plot_results(figsize=(12, 8), plot_correlations=False)
 backtester.log_results()
