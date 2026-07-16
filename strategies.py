0a4113c80e2a9a68a312bc40a68c41319187c944
Juan Santhosh <juansanthosh@gmail.com>
2026-07-16 10:44:19 +0530

    Add docstrings to strategies

--- strategies.py
+++ strategies.py
@@ -4,6 +4,10 @@ from dataclasses import dataclass
 import pandas as pd
 
 class Strategy(ABC):
+    """
+    Abstract class which respresents a vectorised trading strategy to be evaluated with the backtester.
+    Consists of a name and a signal function to return target positions over time given price data.
+    """
     @property
     @abstractmethod
     def name(self) -> str:
@@ -11,10 +15,18 @@ class Strategy(ABC):
 
     @abstractmethod
     def generate_signals(self, df: pd.DataFrame) -> pd.Series:
+        """Vectorised signal functions which takes a historical price DataFrame and returns a signal Series.
+
+        Args:
+            df (pd.DataFrame): DataFrame containing historical close price data.
+        
+        Returns:
+            pd.Series: Series of target positions over time corresponding to each index in the DataFrame.
+        """
         ...
 
 @dataclass(slots=True)
-class BuyAndHold(Strategy):
+class BuyAndHold(Strategy): # Example strategy
     amount: float = 1.0
 
     @property
@@ -22,4 +34,5 @@ class BuyAndHold(Strategy):
         return f"Buy & Hold {self.amount} Tokens"
     
     def generate_signals(self, df: pd.DataFrame) -> pd.Series:
-        return pd.Series(self.amount, index=range(len(df)))
+        # Hold {amount} tokens over entire window.
+        return pd.Series(self.amount, index=range(len(df))) 
